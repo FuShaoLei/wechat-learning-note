47a0e88f26d03c3c02d2f4a99d0032ca3a104805
fushaolei <fushaolei1999@gmail.com>
2023-04-20 14:22:37 +0800

    bakcup

--- demo-student/miniprogram/pages/homework-add/index.ts
+++ demo-student/miniprogram/pages/homework-add/index.ts
@@ -90,9 +90,7 @@ Page({
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const day = String(date.getDate()).padStart(2, '0');
         const dateString = `${year}/${month}/${day}`;
-        this.setData({
-            dateText: dateString
-        });
+        this.setData({dateText: dateString});
 
 
         console.log("homework-add onload");
