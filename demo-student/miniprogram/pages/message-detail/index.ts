4f2159b4594aa18fd3c27af62eceb872e998e61f
fushaolei <fushaolei1999@gmail.com>
2023-04-20 18:00:47 +0800

    backup

--- demo-student/miniprogram/pages/message-detail/index.ts
+++ demo-student/miniprogram/pages/message-detail/index.ts
@@ -6,75 +6,77 @@ var windowHeight = wx.getSystemInfoSync().windowHeight;
 var keyHeight = 0;
 
 Page({
-  data: {
+    data: {
 
 
 
-    chatData: [{
-      status: 0,
-      text: "你好啊？"
+        chatData: [{
+            status: 0,
+            text: "你好啊？"
+        },
+        {
+            status: 1,
+            text: "我很好啊"
+        }
+        ],
+        scrollHeight: '100vh',
+        inputBottom: '0'
+
     },
-    {
-      status: 1,
-      text: "我很好啊"
-    }
-    ],
-    scrollHeight: '100vh',
-    inputBottom: '0'
+    onLoad() {
+        // @ts-ignore
+        if (wx.getUserProfile) {
+            this.setData({
+                canIUseGetUserProfile: true
+            })
+        }
+    },
 
-  },
-  onLoad() {
-    // @ts-ignore
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
+    focus(e: any) {
+        console.log("message-detail focus");
+        console.log(e);
 
-  focus(e:any){
-    console.log("message-detail focus");
-    console.log(e);
 
-    
-    keyHeight = e.detail.height;
-    this.setData({
-      scrollHeight: (windowHeight - keyHeight) + 'px'
-    });
-    this.setData({
-      toView: 'msg-' + (this.data.chatData.length - 1),
-      inputBottom: keyHeight + 'px'
-    })
-    //计算msg高度
-    // calScrollHeight(this, keyHeight);
+        keyHeight = e.detail.height;
+        this.setData({
+            scrollHeight: (windowHeight - keyHeight) + 'px'
+        });
+        this.setData({
+            toView: 'msg-' + (this.data.chatData.length - 1),
+            inputBottom: keyHeight + 'px'
+        })
+        //计算msg高度
+        // calScrollHeight(this, keyHeight);
 
-  },
-  //失去聚焦(软键盘消失)
-  blur(e:any) {
-    this.setData({
-      scrollHeight: '100vh',
-      inputBottom: '0'
-    })
-    this.setData({
-      toView: 'msg-' + (this.data.chatData.length - 1)
-    })
+    },
+    //失去聚焦(软键盘消失)
+    blur(e: any) {
+        console.log("message-detail blur");
+        console.log(e);
+        this.setData({
+            scrollHeight: '100vh',
+            inputBottom: '0'
+        });
+        this.setData({
+            toView: 'msg-' + (this.data.chatData.length - 1)
+        });
 
-  },
+    },
 
-  /**
-   * 发送点击监听
-   */
-  sendClick(e:any) {
-    console.log("message-detail sendClick");
-    console.log(e);
+    /**
+     * 发送点击监听
+     */
+    sendClick(e: any) {
+        console.log("message-detail sendClick");
+        console.log(e);
 
-    this.data.chatData.push({
-      status: 1,
-      text: e.detail.value
-    });
-    this.setData({
-      chatData: this.data.chatData,
-      inputVal:''
-    });
-  },
+        this.data.chatData.push({
+            status: 1,
+            text: e.detail.value
+        });
+        this.setData({
+            chatData: this.data.chatData,
+            inputVal: ''
+        });
+    },
 })
